49d6d53448215d0599dfc9441511984cc86cea9b
G Spencer Fabian <gfabian@yp.com>
2017-06-05 08:15:03 -0700

    adding more comments

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 			log.Fatalln(err)
 		}
 
+		// this newegg api is for their mobile app. We need to spoof the user agent to look like a mobile device.
 		req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1")
 
 		resp, err := client.Do(req)
